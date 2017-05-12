902406bb4455e72828aba09c7f4ce1ae05c7051e
Muhammad Javed <daudjaved1@gmail.com>
2017-05-12 15:45:49 -0400

    checkpoint 30 Fixed messages

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,13 +1,14 @@
 (function() {
-    function RoomCtrl(Room) {
-       this.rooms = Room.all;     
+    function RoomCtrl(Room,Message,$scope) {
+       this.rooms = Room.all;   
+       this.changeRoom = function(newRoom){
+            $scope.currentRoom = newRoom ;
+            $scope.messages = Message.getByRoomId(newRoom.$id);
+        }
     }
-    this.changeRoom = function(newRoom){
-        $scope.currentRoom = newRoom ;
-        $scope.messages = Message.getByRoomId($scope.currentRoom.$id);
-    }
+   
 
     angular
         .module('blocChat')
-        .controller('RoomCtrl', ["Room",RoomCtrl]);
+        .controller('RoomCtrl', ["Room","Message","$scope",RoomCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,11 +1,11 @@
 (function() {
   function Message($firebaseArray) {
     var Message = {};
-    var ref = firebase.database().ref().child("messages");
+    var ref = firebase.database().ref().child("messages");  
     var messages = $firebaseArray(ref);
       
     Message.getByRoomId = function(roomId) {
-       $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
+       return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
         
     };  
       
